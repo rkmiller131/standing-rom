0860c13346725cd60fc3b6a24abe807be552f315
Rachel Miller <rkmiller131@gmail.com>
2024-04-19 11:05:16 -0700

    Refined Arm Angle helper, added jsdocs

--- src/avatar/Avatar.tsx
+++ src/avatar/Avatar.tsx
@@ -2,7 +2,6 @@ import { VRM, VRMLoaderPlugin } from '@pixiv/three-vrm'
 import { useFrame } from '@react-three/fiber'
 import { useLayoutEffect, Suspense, useState } from 'react'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import calculateArmAngles from '../helpers/calculateArmAngles';
 
 interface AvatarProps {
   setAvatarModel: (vrm: VRM) => void;
@@ -17,8 +16,8 @@ export default function Avatar({ setAvatarModel, avatar }: AvatarProps) {
           return new VRMLoaderPlugin(parser);
         });
         loader.load(
-          'https://cdn.glitch.com/29e07830-2317-4b15-a044-135e73c7f840%2FAshtra.vrm?v=1630342336981',
-          // '/Man.vrm',
+          // 'https://cdn.glitch.com/29e07830-2317-4b15-a044-135e73c7f840%2FAshtra.vrm?v=1630342336981',
+          '/Man.vrm',
           (gltf) => {
             const vrm = gltf.userData.vrm;
             setAvatarModel(vrm);
@@ -30,11 +29,12 @@ export default function Avatar({ setAvatarModel, avatar }: AvatarProps) {
       }, [setAvatarModel]);
 
     useFrame(({ gl, scene, camera }) => {
-      if (avatar.current) {
-        // const {leftArmAngle, rightArmAngle} = calculateArmAngles(avatar);
-        // // console.log('~~ LEFT ARM ANGLE ', leftArmAngle)
-        // console.log('~~ right arm angle ', rightArmAngle)
-      }
+      // SAVED FOR REFERENCE ON GETTING THE ARM ANGLES - PUT IN A SYSTEM CHECKING 1x/60 frames
+      // if (avatar.current) {
+      //   const {leftArmAngle, rightArmAngle} = calculateArmAngles(avatar);
+      //   // console.log('~~ LEFT ARM ANGLE ', leftArmAngle)
+      //   console.log('~~ right arm angle ', rightArmAngle)
+      // }
         gl.render(scene, camera);
     }, 1);
 
