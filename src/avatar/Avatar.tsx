58ab12fffeb17c45a59c037cbc7bc24f66ba8e8e
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-04-19 11:48:59 -0700

    spatial text

--- src/avatar/Avatar.tsx
+++ src/avatar/Avatar.tsx
@@ -1,49 +1,63 @@
-import { VRM, VRMLoaderPlugin } from '@pixiv/three-vrm'
-import { useFrame } from '@react-three/fiber'
-import { useLayoutEffect, Suspense, useState } from 'react'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { VRM, VRMLoaderPlugin } from "@pixiv/three-vrm";
+import { useFrame } from "@react-three/fiber";
+import { useLayoutEffect, Suspense, useState } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import calculateArmAngles from "../helpers/calculateArmAngles";
+import { Text } from "@react-three/drei";
 
 interface AvatarProps {
   setAvatarModel: (vrm: VRM) => void;
   avatar: React.RefObject<VRM>;
 }
 export default function Avatar({ setAvatarModel, avatar }: AvatarProps) {
-    const [avatarLoaded, setAvatarLoaded] = useState(false);
+  const [avatarLoaded, setAvatarLoaded] = useState(false);
+  const [right, setRightArmAngle] = useState(0);
+  const [left, setLeftArmAngle] = useState(0);
 
-    useLayoutEffect(() => {
-        const loader = new GLTFLoader();
-        loader.register((parser) => {
-          return new VRMLoaderPlugin(parser);
-        });
-        loader.load(
-          // 'https://cdn.glitch.com/29e07830-2317-4b15-a044-135e73c7f840%2FAshtra.vrm?v=1630342336981',
-          '/Man.vrm',
-          (gltf) => {
-            const vrm = gltf.userData.vrm;
-            setAvatarModel(vrm);
-            setAvatarLoaded(true);
-          },
-          (progress) => console.log( 'Loading model...', 100.0 * ( progress.loaded / progress.total ), '%' ),
-          (error) => console.error('Error Loading Avatar: ', error)
-        )
-      }, [setAvatarModel]);
+  useLayoutEffect(() => {
+    const loader = new GLTFLoader();
+    loader.register((parser) => {
+      return new VRMLoaderPlugin(parser);
+    });
+    loader.load(
+      "https://cdn.glitch.com/29e07830-2317-4b15-a044-135e73c7f840%2FAshtra.vrm?v=1630342336981",
+      // '/Man.vrm',
+      (gltf) => {
+        const vrm = gltf.userData.vrm;
+        setAvatarModel(vrm);
+        setAvatarLoaded(true);
+      },
+      (progress) =>
+        console.log(
+          "Loading model...",
+          100.0 * (progress.loaded / progress.total),
+          "%"
+        ),
+      (error) => console.error("Error Loading Avatar: ", error)
+    );
+  }, [setAvatarModel]);
 
-    useFrame(({ gl, scene, camera }) => {
-      // SAVED FOR REFERENCE ON GETTING THE ARM ANGLES - PUT IN A SYSTEM CHECKING 1x/60 frames
-      // if (avatar.current) {
-      //   const {leftArmAngle, rightArmAngle} = calculateArmAngles(avatar);
-      //   // console.log('~~ LEFT ARM ANGLE ', leftArmAngle)
-      //   console.log('~~ right arm angle ', rightArmAngle)
-      // }
-        gl.render(scene, camera);
-    }, 1);
+  useFrame(() => {
+    if (avatar.current) {
+      const { leftArmAngle, rightArmAngle } = calculateArmAngles(avatar);
+      setRightArmAngle(rightArmAngle);
+      setLeftArmAngle(leftArmAngle);
+    }
+  });
 
-
-      return (
-        <Suspense fallback={null}>
-            {avatarLoaded && (
-                <primitive object={avatar.current!.scene} scale={[0.85, 0.85, 0.85]} />
-            )}
-        </Suspense>
-      );
+  return (
+    <Suspense fallback={null}>
+      {avatarLoaded && (
+        <>
+          <primitive
+            object={avatar.current!.scene}
+            scale={[0.85, 0.85, 0.85]}
+          />
+          <Text position={[0, 2, -2]} scale={0.35} color="black">
+            {right} and {left}
+          </Text>
+        </>
+      )}
+    </Suspense>
+  );
 }
--- src/helpers/calculateArmAngles.ts
+++ src/helpers/calculateArmAngles.ts
@@ -1,5 +1,5 @@
-import { VRM } from '@pixiv/three-vrm'
-import { Euler, MathUtils, Quaternion } from 'three'
+import { VRM } from "@pixiv/three-vrm";
+import { Euler, MathUtils, Quaternion } from "three";
 
 const ninetyDegreeAdjust = new Euler(0, 0, Math.PI / 2);
 const rotatedLeftArm = new Quaternion().setFromEuler(ninetyDegreeAdjust);
@@ -14,22 +14,62 @@ const rightArmAngleEuler = new Euler();
  * @returns {{ leftArmAngle: number, rightArmAngle: number }} The angles of the left and right arms in degrees.
  */
 
-export default function calculateArmAngles(vrm: React.RefObject<VRM>): { leftArmAngle: number, rightArmAngle: number } {
+export default function calculateArmAngles(vrm: React.RefObject<VRM>): {
+  leftArmAngle: number;
+  rightArmAngle: number;
+} {
+  // const leftArmQuat = vrm.current!.humanoid.normalizedHumanBones.leftUpperArm.node.quaternion;
+  // const rightArmQuat = vrm.current!.humanoid.normalizedHumanBones.rightUpperArm.node.quaternion;
 
-    const leftArmQuat = vrm.current!.humanoid.humanBones.leftUpperArm.node.quaternion;
-    const rightArmQuat = vrm.current!.humanoid.humanBones.rightUpperArm.node.quaternion;
+  // // rotatedLeftArm.setFromEuler(ninetyDegreeAdjust);
+  // // rotatedRightArm.setFromEuler(ninetyDegreeAdjust);
 
-    const updatedLeftArmQuat = leftArmQuat.clone().multiply(rotatedLeftArm);
-    const updatedRightArmQuat = rightArmQuat.clone().multiply(rotatedRightArm);
+  // const updatedLeftArmQuat = leftArmQuat!.multiply(rotatedLeftArm);
+  // const updatedRightArmQuat = rightArmQuat!.multiply(rotatedRightArm);
 
-    leftArmAngleEuler.setFromQuaternion(updatedLeftArmQuat);
-    rightArmAngleEuler.setFromQuaternion(updatedRightArmQuat);
+  // leftArmAngleEuler.setFromQuaternion(updatedLeftArmQuat);
+  // rightArmAngleEuler.setFromQuaternion(updatedRightArmQuat);
+  // // console.log('~~ left Arm quaternion normalized vs normal bone quat ', leftArmQuat)
+  // // console.log('~~ not normalized lefty ', vrm.current?.humanoid.humanBones.leftUpperArm.node.quaternion)
 
-    const leftArmAngleFlipped = Math.round(MathUtils.radToDeg(leftArmAngleEuler.z));
-    const rightArmAngleFlipped = Math.round(180 - MathUtils.radToDeg(rightArmAngleEuler.z));
+  // leftArmAngleEuler.setFromQuaternion(leftArmQuat);
+  // rightArmAngleEuler.setFromQuaternion(rightArmQuat);
 
-    const leftArmAngle = Math.abs(180 - leftArmAngleFlipped);
-    const rightArmAngle = Math.abs(180 - rightArmAngleFlipped);
+  // const leftArmAngle = Math.round(MathUtils.radToDeg(leftArmAngleEuler.z));
+  // const rightArmAngle = Math.round(180 - MathUtils.radToDeg(rightArmAngleEuler.z));
 
-    return { leftArmAngle, rightArmAngle };
+  // -----------------------------------------------------------------------------------------
+  // const leftArmQuat = vrm.current!.humanoid.humanBones.leftUpperArm.node.quaternion;
+  // const leftArmEuler = new Euler().setFromQuaternion(leftArmQuat!);
+
+  // const leftArmAngle = Math.round(MathUtils.radToDeg(leftArmEuler.x));
+
+  // const rightArmQuat = vrm.current!.humanoid.humanBones.rightUpperArm.node.quaternion;
+  // const rightArmEuler = new Euler().setFromQuaternion(rightArmQuat);
+
+  // const rightArmAngle = Math.round(MathUtils.radToDeg(rightArmEuler.x));
+
+  // -------------------------------------------------------------------------------------------
+
+  const ninetyDegreeAdjust = new Euler(0, 0, Math.PI / 2);
+  const leftArmQuat =
+    vrm.current!.humanoid.humanBones.leftUpperArm.node.quaternion;
+  const rightArmQuat =
+    vrm.current!.humanoid.humanBones.rightUpperArm.node.quaternion;
+
+  const rotatedLeftArm = new Quaternion().setFromEuler(ninetyDegreeAdjust);
+  const rotatedRightArm = new Quaternion().setFromEuler(ninetyDegreeAdjust);
+
+  const updatedLeftArmQuat = leftArmQuat.clone().multiply(rotatedLeftArm);
+  const updatedRightArmQuat = rightArmQuat.clone().multiply(rotatedRightArm);
+
+  const leftArmAngleEuler = new Euler().setFromQuaternion(updatedLeftArmQuat);
+  const rightArmAngleEuler = new Euler().setFromQuaternion(updatedRightArmQuat);
+
+  const leftArmAngle = Math.round(MathUtils.radToDeg(leftArmAngleEuler.z));
+  const rightArmAngle = Math.round(
+    180 - MathUtils.radToDeg(rightArmAngleEuler.z)
+  );
+
+  return { leftArmAngle, rightArmAngle };
 }
