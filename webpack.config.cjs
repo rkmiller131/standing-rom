c087eb2a1e9bbbc8a436cc0b632b6879a87d2245
rkmiller131 <rkmiller131@gmail.com>
2024-04-24 12:49:07 -0700

    Webpack now successfully builds

---
+++ webpack.config.cjs
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+module.exports = {
+ mode: 'production',
+ entry: './src/main.tsx',
+ output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+ },
+ resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+ },
+ module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['file-loader']
+      },
+    ],
+ }
+};
