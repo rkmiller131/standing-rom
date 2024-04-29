d70d773519098c2a59a6a20913c55ddce6416479
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-04-29 13:17:44 -0700

    lighting tweaks

--- src/environment/Office.tsx
+++ src/environment/Office.tsx
@@ -1,47 +1,68 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { gltfLoader as loader, dracoLoader, GLTF } from '../THREE_Interface'
-import { Suspense, useEffect, useState } from 'react'
-import debounce from '../ecs/helpers/debounce'
+import { gltfLoader as loader, dracoLoader, GLTF } from "../THREE_Interface";
+import { Suspense, useEffect, useState } from "react";
+import debounce from "../ecs/helpers/debounce";
 
 interface OfficeProps {
   setEnvironmentModel: (gltf: GLTF) => void;
   environment: React.RefObject<GLTF>;
 }
 
-export default function Office({ setEnvironmentModel, environment }: OfficeProps) {
+export default function Office({
+  setEnvironmentModel,
+  environment,
+}: OfficeProps) {
   const [officeLoaded, setOfficeLoaded] = useState(false);
   const updateProgress = debounce((loadedPercentage) => {
     // could set a state here instead, for loading screen in the future
-    console.log('Loading Environment: ', loadedPercentage + '%');
-  }, 100)
+    console.log("Loading Environment: ", loadedPercentage + "%");
+  }, 100);
 
   useEffect(() => {
-    dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
-    loader.setDRACOLoader( dracoLoader );
+    dracoLoader.setDecoderPath("https://www.gstatic.com/draco/v1/decoders/");
+    loader.setDRACOLoader(dracoLoader);
 
     loader.load(
-      'https://cdn.glitch.global/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/office.glb?v=1714005557457',
+      "/office-transformed.glb",
       (gltf) => {
         setEnvironmentModel(gltf);
         setOfficeLoaded(true);
+        console.log(gltf.scene.children[0].children[0].name); //Cube_1
+        console.log(gltf.scene.children[0].children[0].material.name); //Material.003
+        //set mats up for receive and cast shadow
+        /* 
+          Cube_1 - Cube_13
+
+          ['Material.003']: THREE.MeshStandardMaterial
+          ['Material.005']: THREE.MeshStandardMaterial
+          PaletteMaterial002: THREE.MeshStandardMaterial //Cast
+          laminate_floor_02: THREE.MeshStandardMaterial //Receive
+          PaletteMaterial004: THREE.MeshStandardMaterial //Cast
+          ['Material.007']: THREE.MeshStandardMaterial
+          ['Material.010']: THREE.MeshStandardMaterial
+          PaletteMaterial005: THREE.MeshStandardMaterial
+          small_wooden_table_01: THREE.MeshStandardMaterial //Cast
+          steel_frame_shelves_02: THREE.MeshStandardMaterial //Cast
+          PottedPlant_04: THREE.MeshStandardMaterial //Cast
+          PaletteMaterial001: THREE.MeshStandardMaterial
+          PaletteMaterial003: THREE.MeshStandardMaterial
+        */
       },
       (progress) => {
         const loadedPercentage = 100 * (progress.loaded / progress.total);
         updateProgress(loadedPercentage);
       },
       (error) => {
-        console.error("Error Loading Environment: ", error)
+        console.error("Error Loading Environment: ", error);
       }
-    )
+    );
   }, []);
 
   return (
     <Suspense fallback={null}>
       {officeLoaded && (
         <>
-          <primitive
-            object={environment.current!.scene}
-          />
+          <primitive object={environment.current!.scene} />
         </>
       )}
     </Suspense>
--- src/environment/lights/helpers/createLights.ts
+++ src/environment/lights/helpers/createLights.ts
@@ -1,16 +1,17 @@
-import { PointLight, Scene } from 'three'
+import { PointLight, Scene } from "three";
 
 export const bulbLuminousPowers = {
-    "1000W": 110000,
-    "300W": 3500,
-    "100W": 1700,
-    "60W": 800,
-    "40W": 400,
-    "25W": 180,
-    "10W": 120,
-    "4W": 20,
-    Off: 0,
-  };
+  "1000W": 110000,
+  "300W": 3500,
+  "100W": 1700,
+  "60W": 800,
+  "40W": 400,
+  "25W": 180,
+  "10W": 120,
+  "6W": 60,
+  "4W": 20,
+  Off: 0,
+};
 
 export const createBulbLight = (
   scene: Scene,
@@ -19,7 +20,6 @@ export const createBulbLight = (
 ) => {
   const bulbLight = new PointLight(0xffffff, 1, 100, 2);
   bulbLight.position.set(...position);
-  bulbLight.castShadow = true;
 
   scene.add(bulbLight);
 
