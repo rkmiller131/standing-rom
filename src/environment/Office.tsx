6c57e5d554d84e30746ccedd9188a48ae3687eef
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-04-30 13:51:35 -0700

    prep for move to main

--- src/environment/Office.tsx
+++ src/environment/Office.tsx
@@ -19,10 +19,8 @@ export default function Office({
   }, 100);
 
   function updateShadows(gltf: GLTF) {
-    // Directly iterate over the meshes in the scene
     gltf.scene.traverse((object) => {
       if (object) {
-        // Check if the mesh name matches any of the specified names for castShadow
         const isCastShadow =
           object.name === "Cube_3" ||
           object.name === "Cube_7" ||
@@ -31,7 +29,6 @@ export default function Office({
           object.name === "Cube_10" ||
           object.name === "Cube_11" ||
           object.name === "Cube_12";
-        // Check if the mesh name matches any of the specified names for receiveShadow
         const isReceiveShadow =
           object.name === "Cube_13" ||
           object.name === "Cube_3" ||
@@ -40,7 +37,6 @@ export default function Office({
           object.name === "Cube_7" ||
           object.name === "Cube_8";
 
-        // Apply castShadow and receiveShadow based on the conditions
         if (isCastShadow) {
           object.castShadow = true;
           console.log(`Set castShadow for object: ${object.name}`);
@@ -62,28 +58,8 @@ export default function Office({
       "/office-transformed.glb",
       (gltf) => {
         setEnvironmentModel(gltf);
-        setOfficeLoaded(true);
-
-        // console.log(gltf.scene.children[0].children[0].material.name);
-        //set mats up for receive and cast shadow
-        /* 
-          Cube_1 - Cube_13 map to each of these mats sequentially...
-
-          ['Material.003']: THREE.MeshStandardMaterial  1
-          ['Material.005']: THREE.MeshStandardMaterial  2 //Ceiling
-          PaletteMaterial002: THREE.MeshStandardMaterial //Cast   3
-          laminate_floor_02: THREE.MeshStandardMaterial //Receive   4
-          PaletteMaterial004: THREE.MeshStandardMaterial //Cast    5 
-          ['Material.007']: THREE.MeshStandardMaterial  6
-          ['Material.010']: THREE.MeshStandardMaterial  7
-          PaletteMaterial005: THREE.MeshStandardMaterial    8
-          small_wooden_table_01: THREE.MeshStandardMaterial //Cast  9
-          steel_frame_shelves_02: THREE.MeshStandardMaterial //Cast   10
-          PottedPlant_04: THREE.MeshStandardMaterial //Cast   11
-          PaletteMaterial001: THREE.MeshStandardMaterial  12
-          PaletteMaterial003: THREE.MeshStandardMaterial  13
-        */
         updateShadows(gltf);
+        setOfficeLoaded(true);
       },
       (progress) => {
         const loadedPercentage = 100 * (progress.loaded / progress.total);
