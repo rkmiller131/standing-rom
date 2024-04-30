277a6ab76c6dda390b692ece761a2da9bc886753
rkmiller131 <rkmiller131@gmail.com>
2024-04-30 15:20:53 -0700

    Separation of concerns - environment helper

--- src/environment/Office.tsx
+++ src/environment/Office.tsx
@@ -2,6 +2,7 @@
 import { gltfLoader as loader, dracoLoader, GLTF } from "../THREE_Interface";
 import { Suspense, useEffect, useState } from "react";
 import debounce from "../ecs/helpers/debounce";
+import updateShadows from "./lights/helpers/updateShadows";
 
 interface OfficeProps {
   setEnvironmentModel: (gltf: GLTF) => void;
@@ -13,42 +14,12 @@ export default function Office({
   environment,
 }: OfficeProps) {
   const [officeLoaded, setOfficeLoaded] = useState(false);
+
   const updateProgress = debounce((loadedPercentage) => {
     // could set a state here instead, for loading screen in the future
     console.log("Loading Environment: ", loadedPercentage + "%");
   }, 100);
 
-  function updateShadows(gltf: GLTF) {
-    gltf.scene.traverse((object) => {
-      if (object) {
-        const isCastShadow =
-          object.name === "Cube_3" ||
-          object.name === "Cube_7" ||
-          object.name === "Cube_8" ||
-          object.name === "Cube_9" ||
-          object.name === "Cube_10" ||
-          object.name === "Cube_11" ||
-          object.name === "Cube_12";
-        const isReceiveShadow =
-          object.name === "Cube_13" ||
-          object.name === "Cube_3" ||
-          object.name === "Cube_4" ||
-          object.name === "Cube_5" ||
-          object.name === "Cube_7" ||
-          object.name === "Cube_8";
-
-        if (isCastShadow) {
-          object.castShadow = true;
-          console.log(`Set castShadow for object: ${object.name}`);
-        }
-        if (isReceiveShadow) {
-          object.receiveShadow = true;
-          console.log(`Set receiveShadow for object: ${object.name}`);
-        }
-      }
-    });
-    return gltf;
-  }
 
   useEffect(() => {
     dracoLoader.setDecoderPath("https://www.gstatic.com/draco/v1/decoders/");
