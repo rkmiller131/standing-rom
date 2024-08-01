03edea82d715006e5d4ecbbccce175a922a618d9
rkmiller131 <rkmiller131@gmail.com>
2024-08-01 09:17:18 -0700

    Added better office splash loading screen

--- src/components/ui/LoadingScreen.tsx
+++ src/components/ui/LoadingScreen.tsx
@@ -11,7 +11,7 @@ type SplashType = {
   [key in EnvironmentSelectionType]: string
 }
 const splash: SplashType = {
-  'Indoor Office': 'https://cdn.glitch.global/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/OfficeSplash.JPG?v=1722474761627',
+  'Indoor Office': 'https://cdn.glitch.global/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/OfficeSplash.png?v=1722528887283',
   'Outdoors': 'https://cdn.glitch.global/c4f540ac-7f7c-41b2-ae89-9e2617351aa6/OutdoorSplash.png?v=1722473814520',
   '': 'https://cdn.glitch.global/c4f540ac-7f7c-41b2-ae89-9e2617351aa6/loadingStill.JPG?v=1722473502361'
 }
