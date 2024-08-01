d0f56cbbca02a0bb3b57b0f9b6de2539cd89c998
rkmiller131 <rkmiller131@gmail.com>
2024-08-01 13:53:16 -0700

    Added updated grass shaders

--- src/components/environment/outdoors/shaders/grassFragmentShader.ts
+++ src/components/environment/outdoors/shaders/grassFragmentShader.ts
@@ -1,18 +1,22 @@
 // glsl
 export const grassFragmentShader = `
-    uniform sampler2D uCloud;
+  uniform sampler2D uCloud;
 
-    varying vec3 vPosition;
-    varying vec2 vUv;
-    varying vec3 vNormal;
+  varying vec3 vPosition;
+  varying vec2 vUv;
+  varying vec3 vNormal;
 
-    vec3 green = vec3(0.4, 0.7, 0.2);
+  vec3 green = vec3(0.408,0.522,0.078);
+  vec3 hay = vec3(0.584,0.639,0.165);
 
-    void main() {
-      vec3 color = mix(green * 0.7, green, vPosition.y);
-      color = mix(color, texture2D(uCloud, vUv).rgb, 0.4);
+  void main() {
+    vec3 color = green;
 
-      float lighting = normalize(dot(vNormal, vec3(10)));
-      gl_FragColor = vec4(color + lighting * 0.03, 1.0);
-    }
+    vec3 map = mix(hay, texture2D(uCloud, vUv).rgb, 0.5);
+
+    color = mix(color, map, 0.5);
+
+    float lighting = normalize(dot(vNormal, vec3(10)));
+    gl_FragColor = vec4(color + lighting * 0.03, 1.0);
+  }
 `;
