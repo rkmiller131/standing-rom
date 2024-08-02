99a90fc67008edee4d81ebef24b30122f461b48d
rkmiller131 <rkmiller131@gmail.com>
2024-08-02 15:31:27 -0700

    Checkpoint - testing tony's bubbles

--- src/components/ui/ScoreDisplay.tsx
+++ src/components/ui/ScoreDisplay.tsx
@@ -12,8 +12,11 @@ export default function ScoreDisplay() {
   } = useHookstateGetters();
   const gameState = useGameState();
   const bubbleAnimRef = useRef<HTMLVideoElement | null>(null);
+  const idleAnimRef = useRef<HTMLVideoElement | null>(null);
   // const fireAnimRef = useRef<HTMLVideoElement | null>(null);
   const [streaking, setStreaking] = useState(false);
+  const [idleVisibility, setIdleVisibility] = useState(true);
+
 
   const popped = getPoppedBubbleCount();
   const total = getTotalBubbleCount();
@@ -30,19 +33,36 @@ export default function ScoreDisplay() {
 
   useEffect(() => {
     const bubbleAnimation = bubbleAnimRef.current;
-    if (bubbleAnimation) bubbleAnimation.play();
+    if (!bubbleAnimation) return;
+    setIdleVisibility(false);
+    bubbleAnimation.play();
+
+    const handleAnimationEnd = () => {
+      setIdleVisibility(true);
+    }
+
+    bubbleAnimation.addEventListener('ended', handleAnimationEnd);
+
+    return () => {
+      if (bubbleAnimation) bubbleAnimation.removeEventListener('ended', handleAnimationEnd);
+    };
   }, [gameState.score.popped])
 
   return (
     <div id="score-ui-bar">
       <div className="score-ui-container">
-        <video ref={bubbleAnimRef} muted>
-          <source src="https://cdn.glitch.global/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/bubblesPopping.mp4?v=1722621583923" type="video/mp4"/>
+        <video ref={idleAnimRef} muted autoPlay loop className={`${idleVisibility ? 'animation' : 'animation hide'}`}>
+          <source src="https://cdn.glitch.global/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/BubbleIdle.webm?v=1722629899865" type="video/webm"/>
+        </video>
+        <video ref={bubbleAnimRef} muted className="animation popping-bubble">
+          <source src="https://cdn.glitch.global/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/SingleBubblePopAnim.webm?v=1722628545958" type="video/webm"/>
         </video>
         <div className="score-tracker">
           <div className="placeholder-bubble-effect" />
-          <span className={streaking ? 'streak-effect score-popped' : 'score-popped'}>{popped}</span> /
-          <span className="score-possible">{total} Bubbles Popped</span>
+          <div className="player-score-container">
+            <span className={streaking ? 'streak-effect score-popped' : 'score-popped'}>{popped}</span> /
+            <span className="score-possible">{total} Bubbles Popped</span>
+          </div>
         </div>
       </div>
     </div>
