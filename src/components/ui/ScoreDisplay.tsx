d8d36e25f55bde30ba67ce92463d17168c229dc5
rkmiller131 <rkmiller131@gmail.com>
2024-08-03 10:24:41 -0700

    Fixed calibrating visual bug and setup score ui

--- src/components/ui/ScoreDisplay.tsx
+++ src/components/ui/ScoreDisplay.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useRef, useState } from 'react';
 import useHookstateGetters from '../../interfaces/Hookstate_Interface';
 import { useGameState } from '../../hookstate-store/GameState';
@@ -17,7 +18,6 @@ export default function ScoreDisplay() {
   const [streaking, setStreaking] = useState(false);
   const [idleVisibility, setIdleVisibility] = useState(true);
 
-
   const popped = getPoppedBubbleCount();
   const total = getTotalBubbleCount();
   const currentStreak = getCurrentStreak();
@@ -28,39 +28,41 @@ export default function ScoreDisplay() {
     } else {
       setStreaking(false);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [gameState.score.currentStreak])
 
   useEffect(() => {
-    const bubbleAnimation = bubbleAnimRef.current;
-    if (!bubbleAnimation) return;
+    const popAnimation = bubbleAnimRef.current;
+    const idleAnimation = idleAnimRef.current;
+    if (!popAnimation || !idleAnimation || popped === 0) return;
     setIdleVisibility(false);
-    bubbleAnimation.play();
+    popAnimation.play();
 
     const handleAnimationEnd = () => {
       setIdleVisibility(true);
     }
 
-    bubbleAnimation.addEventListener('ended', handleAnimationEnd);
+    popAnimation.addEventListener('ended', handleAnimationEnd);
 
     return () => {
-      if (bubbleAnimation) bubbleAnimation.removeEventListener('ended', handleAnimationEnd);
+      if (popAnimation) popAnimation.removeEventListener('ended', handleAnimationEnd);
     };
   }, [gameState.score.popped])
 
   return (
     <div id="score-ui-bar">
-      <div className="score-ui-container">
+      <div className="bubble-animations">
         <video ref={idleAnimRef} muted autoPlay loop className={`${idleVisibility ? 'animation' : 'animation hide'}`}>
           <source src="https://cdn.glitch.global/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/BubbleIdle.webm?v=1722629899865" type="video/webm"/>
         </video>
-        <video ref={bubbleAnimRef} muted className="animation popping-bubble">
+        <video ref={bubbleAnimRef} muted className="animation popping-bubble" onCanPlay={() => bubbleAnimRef.current!.playbackRate = 5.0}>
           <source src="https://cdn.glitch.global/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/SingleBubblePopAnim.webm?v=1722628545958" type="video/webm"/>
         </video>
+      </div>
+      <div className="score-ui-container">
         <div className="score-tracker">
-          <div className="placeholder-bubble-effect" />
           <div className="player-score-container">
-            <span className={streaking ? 'streak-effect score-popped' : 'score-popped'}>{popped}</span> /
+            <span className={streaking ? 'streak-effect score-popped' : 'score-popped'}>{popped}</span>
+            <span style={{color: 'white', fontSize: '1.5rem'}}>/</span>
             <span className="score-possible">{total} Bubbles Popped</span>
           </div>
         </div>
