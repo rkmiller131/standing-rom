5ca88fb7a321af761d4e070e8d5629cca4a536f2
rkmiller131 <rkmiller131@gmail.com>
2024-08-06 09:28:46 -0700

    Added score progress bar - progress of popped/tot

--- src/components/ui/player-score/PossibleScore.tsx
+++ src/components/ui/player-score/PossibleScore.tsx
@@ -1,8 +1,17 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
 import useHookstateGetters from '../../../interfaces/Hookstate_Interface';
 
 export default function PossibleScore() {
-  const { getTotalBubbleCount } = useHookstateGetters();
+  const { getTotalBubbleCount, getPoppedBubbleCount } = useHookstateGetters();
   const total = getTotalBubbleCount();
+  const popped = getPoppedBubbleCount();
+  const chunks = Array.from({ length: 5 }, (_, i) => i + 1);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    setProgress(Math.floor((popped / total) * 100));
+  }, [popped])
 
   return (
     <div className="score-ui-container">
@@ -10,8 +19,16 @@ export default function PossibleScore() {
         <source src="https://cdn.glitch.global/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/ScoreBGAnimation.mp4?v=1722725388497" type="video/mp4"/>
       </video>
       <div className="score-content">
-        <span className="score-divider">/</span>
-        <span className="score-possible">{total} Bubbles Popped</span>
+        <div className="score-possible-container">
+          <span className="score-divider">/</span>
+          <span className="score-possible">{total} Bubbles Popped</span>
+        </div>
+        <div className="score-progress-bar">
+          <div className="colored-bar" style={{width: `${progress}%`}}/>
+          {chunks.map((chunk) =>
+            <div className="chunk" key={chunk}/>
+          )}
+        </div>
       </div>
     </div>
   )
