413b22e68e0e12dd40ca16b824e2f1f861d8bad1
rkmiller131 <rkmiller131@gmail.com>
2024-08-07 14:11:59 -0700

    Added avatar with hair model

--- src/utils/cdn-links/models.ts
+++ src/utils/cdn-links/models.ts
@@ -1,4 +1,4 @@
-export const avatarModel = 'https://cdn.glitch.global/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/Man-Compressed.vrm?v=1715274436489';
+export const avatarModel = 'https://cdn.glitch.me/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/ManAvatar_Hair.vrm?v=1723065041817';
 
 export const officeModel = 'https://cdn.glitch.global/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/officeDoor-transformed.glb?v=1720632603947';
 
