a361e2c21ad46bde6c942d1e479530e7a4038a31
rkmiller131 <rkmiller131@gmail.com>
2024-08-12 13:36:54 -0700

    Added inactive bubble shader material

---
+++ src/components/bubble/materials/BubbleMaterial.tsx
@@ -0,0 +1,76 @@
+import { useFrame, useThree } from '@react-three/fiber';
+import { useEffect, useRef } from 'react';
+import { AdditiveBlending, Mesh, RepeatWrapping, ShaderLib, ShaderMaterial, SphereGeometry, TextureLoader, Vector3 } from 'three';
+import { makeBubbleFragShader, makeBubbleVertexShader } from './bubbleShaders';
+
+interface BubbleMaterialProps {
+  active: boolean;
+  position: Vector3;
+}
+export default function BubbleMaterial({active, position}: BubbleMaterialProps) {
+  const textureLoader = new TextureLoader();
+  const bubbleTexture = textureLoader.load(
+    // 'https://cdn.glitch.global/c4f540ac-7f7c-41b2-ae89-9e2617351aa6/perlinNoise.jfif?v=1721870421632'
+    'https://cdn.glitch.global/c4f540ac-7f7c-41b2-ae89-9e2617351aa6/3E95CC_65D9F1_A2E2F6_679BD4-64px.jpeg?v=1722030071519'
+  );
+
+  bubbleTexture.wrapT = RepeatWrapping;
+  bubbleTexture.wrapS = RepeatWrapping;
+
+  const bubbleRef = useRef<Mesh | null>(null);
+  const { scene } = useThree();
+
+  const geometry = new SphereGeometry(0.05, 16, 16);
+  const material = new ShaderMaterial({
+    uniforms: {
+      uPattern: { value: bubbleTexture },
+      uTime: { value: 0 },
+      ...ShaderLib.standard.uniforms,
+    },
+    vertexShader: makeBubbleVertexShader(active),
+    fragmentShader: makeBubbleFragShader(active),
+  });
+
+  useEffect(() => {
+    let bubbleToRemove = null;
+    if (!active) material.blending = AdditiveBlending;
+    material.uniforms.envMap.value = scene.environment;
+    material.needsUpdate = true;
+
+    if (!bubbleRef.current) {
+      const bubble = new Mesh(geometry, material);
+      bubbleRef.current = bubble;
+      bubbleRef.current.position.copy(position);
+      scene.add(bubble);
+      bubbleToRemove = bubble;
+    }
+
+    return () => {
+      if (bubbleToRemove) {
+        scene.remove(bubbleToRemove);
+      }
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [scene]);
+
+  useFrame(({ clock }) => {
+    if (!bubbleRef.current) return;
+    const material = bubbleRef.current.material as ShaderMaterial;
+
+    // update the uniforms
+    if (material.uniforms) {
+      material.uniforms.uTime.value += clock.getDelta();
+    }
+
+    if (active) {
+      const elapsedTime = clock.getElapsedTime();
+      const yOffset = Math.sin(elapsedTime) * 0.0003;
+      const xOffset = Math.cos(elapsedTime) * 0.0003;
+      bubbleRef.current.position.y += yOffset;
+      bubbleRef.current.position.x += xOffset;
+    }
+  });
+
+  return null;
+
+}
---
+++ src/components/bubble/materials/bubbleShaders.ts
@@ -0,0 +1,54 @@
+export function makeBubbleVertexShader(active: boolean) {
+  const vertex = `
+    uniform float uTime;
+    uniform sampler2D uPattern;
+
+    varying vec2 vUv;
+    varying vec3 vPosition;
+    varying vec3 vNormal;
+
+    void main() {
+        vPosition = position;
+        vNormal = normal;
+        vUv = uv;
+
+        float height = smoothstep(0.1, 0.9, texture2D(uPattern, position.yz * 0.02 + vec2(uTime)).x);
+        vec3 displacedPosition = position + normal * height * 0.1;
+
+        // gl_Position = projectionMatrix * modelViewMatrix * vec4(${active ? 'displacedPosition': 'position'}, 1.0);
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `;
+  return vertex;
+}
+
+export function makeBubbleFragShader(active: boolean) {
+  const fragment = `
+    uniform sampler2D uPattern;
+    uniform samplerCube envMap;
+
+    varying vec2 vUv;
+    varying vec3 vPosition;
+    varying vec3 vNormal;
+
+    void main() {
+        vec3 viewDirection = normalize(cameraPosition - vPosition);
+        float fresnel = 1.0 - dot(viewDirection, vNormal);
+
+        float smoothFactor = mix(0.0, 0.75, vUv.y);
+
+        vec3 baseColor = vec3(smoothFactor, 1.0, smoothFactor);
+
+        // vec3 activeColor = vec3(fresnel) * vec3(smoothFactor, 1.0, smoothFactor);
+        vec3 activeColor = texture2D(uPattern, vUv.xy).xyz;
+        vec3 reflectionVector = reflect(-viewDirection, vNormal);
+        vec3 envColor = textureCube(envMap, reflectionVector).rgb;
+        // vec3 activeColor = mix(vec3(0.2, 1.0, 0.2), envColor, 0.3);
+
+        vec3 inactiveColor = vec3(fresnel) * vec3(smoothFactor, smoothFactor, 1.0);
+
+        gl_FragColor = vec4(${active ? 'activeColor, 1.0' : 'inactiveColor, 0.5'});
+    }
+  `;
+  return fragment;
+}
