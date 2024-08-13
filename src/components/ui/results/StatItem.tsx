38fa7b39dd0fca54389fb2112a771a022911bd12
rkmiller131 <rkmiller131@gmail.com>
2024-08-13 16:45:42 -0700

    Added recent performance trends graph

--- src/components/ui/results/StatItem.tsx
+++ src/components/ui/results/StatItem.tsx
@@ -1,15 +1,15 @@
-import { IconName, IconPrefix } from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface StatItemProps {
-  icon: IconName | [IconPrefix, IconName];
+  icon: IconDefinition;
   description: string;
   metric: string;
 }
 export default function StatItem({ icon, description, metric}: StatItemProps) {
   return (
     <div className="stat-item">
-      <FontAwesomeIcon icon={icon} size="3x"/>
+      <FontAwesomeIcon icon={icon} size="2x"/>
       <div className="stat">
         <div className="stat-desc">
           {description}
