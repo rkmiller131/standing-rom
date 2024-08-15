609677d84b85e55692fee75165bdffc976de01cb
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-08-15 11:05:43 -0700

    ui for velocity

--- src/components/ui/ResultsScreen.tsx
+++ src/components/ui/ResultsScreen.tsx
@@ -1,5 +1,4 @@
 import useHookstateGetters from '../../interfaces/Hookstate_Interface';
-import calcAverageVelocity from '../../utils/math/calcAverageVelocity';
 
 import '../../css/ResultsScreen.css';
 
@@ -9,7 +8,7 @@ export default function ResultsScreen() {
     getMaxLeftArmAngle,
     getPoppedBubbleCount,
     getTotalBubbleCount,
-    getPoppedVelocities
+    getPoppedVelocities,
   } = useHookstateGetters();
 
   const maxRightArmAngle = getMaxRightArmAngle();
@@ -17,7 +16,8 @@ export default function ResultsScreen() {
   const popped = getPoppedBubbleCount();
   const totalBubbles = getTotalBubbleCount();
 
-  const avgVelocity = calcAverageVelocity(getPoppedVelocities().slice());
+  const velArray = getPoppedVelocities().slice(-2);
+
   const percentCompletion = Math.round((popped / totalBubbles) * 100);
 
   const handleSubmit = () => {
@@ -25,62 +25,69 @@ export default function ResultsScreen() {
       datePlayed: new Date().toISOString().slice(0, 19),
       percentCompletion,
       bubblesPopped: popped,
-      avgVelocity: Number(avgVelocity),
+      avgLeftVelocity: velArray[0],
+      avgRightVelocity: velArray[1],
       maxLeftAngle: maxLeftArmAngle,
       maxRightAngle: maxRightArmAngle,
-      completed: true
-    }
+      completed: true,
+    };
 
     // For now, just console logging results.
     // Submit to the backend, then redirect to the next game or the uvx dashboard
     console.log('results have been sent! ', results);
     window.location.href = 'https://www.ubiquityvx.com/';
-  }
+  };
 
   const handleReplay = async () => {
-      await caches.keys().then((names) => {
-          return Promise.all(names.map((name) => {
-              return caches.delete(name)
-          }))
-      });
+    await caches.keys().then((names) => {
+      return Promise.all(
+        names.map((name) => {
+          return caches.delete(name);
+        }),
+      );
+    });
 
-      setTimeout(() => {
-          window.location.reload();
-      }, 1000)
-  }
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
+  };
 
   return (
     <>
       <div className="results-screen">
-          <div className="blendHue" />
-          <div className="results-data-container">
-              <h2>Congratulations!</h2>
+        <div className="blendHue" />
+        <div className="results-data-container">
+          <h2>Congratulations!</h2>
 
-              <div className="results-data">
-                  <div className="results-data-item">
-                      <span>AVERAGE POPPING VELOCITY</span>
-                      <span className="results-data-metric">{`${avgVelocity}m/s`}</span>
-                  </div>
-                  <div className="results-data-item">
-                      <span>MAX RIGHT ARM ANGLE</span>
-                      <span className="results-data-metric">{`${maxRightArmAngle}°`}</span>
-                  </div>
-                  <div className="results-data-item">
-                      <span>MAX LEFT ARM ANGLE</span>
-                      <span className="results-data-metric">{`${maxLeftArmAngle}°`}</span>
-                  </div>
-                  <div className="results-data-item">
-                      <span>FINAL SCORE</span>
-                      <span className="results-data-metric">{`${popped}/${totalBubbles}`}</span>
-                  </div>
-              </div>
-              {/* make sure these buttons are only available to the patient to press */}
-              <div className="results-button-container">
-                  <button onClick={handleReplay}>Try Again</button>
-                  <button onClick={handleSubmit}>Submit Results</button>
-              </div>
+          <div className="results-data">
+            <div className="results-data-item">
+              <span>RIGHT VELOCITY</span>
+              <span className="results-data-metric">{`${velArray[0]}m/s`}</span>
+            </div>
+            <div className="results-data-item">
+              <span>LEFT VELOCITY</span>
+              <span className="results-data-metric">{`${velArray[1]}m/s`}</span>
+            </div>
+            <div className="results-data-item">
+              <span>MAX RIGHT ARM ANGLE</span>
+              <span className="results-data-metric">{`${maxRightArmAngle}°`}</span>
+            </div>
+            <div className="results-data-item">
+              <span>MAX LEFT ARM ANGLE</span>
+              <span className="results-data-metric">{`${maxLeftArmAngle}°`}</span>
+            </div>
+            <div className="results-data-item">
+              <span>FINAL SCORE</span>
+              <span className="results-data-metric">{`${popped}/${totalBubbles}`}</span>
+            </div>
           </div>
+          {/* make sure these buttons are only available to the patient to press */}
+          <div className="results-button-container">
+            <button onClick={handleReplay}>Try Again</button>
+            <button onClick={handleSubmit}>Submit Results</button>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
