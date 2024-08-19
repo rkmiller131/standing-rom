c715b751d5b92878d0def12e2ba2ab18a61a635f
rkmiller131 <rkmiller131@gmail.com>
2024-08-19 14:24:27 -0700

    Added back publicly loaded splash/env cards

--- src/utils/cdn-links/images.ts
+++ src/utils/cdn-links/images.ts
@@ -1,9 +1,9 @@
 import { EnvironmentSelectionType } from '../../hookstate-store/Types'
 
-// For the loading screen background splash
+// For the loading screen background splash - kept in /public for faster load times
 export const splash: { [key in EnvironmentSelectionType]: string } = {
-    'Indoor Office': 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/OfficeSplash%20(2).webp?v=1724090985294',
-    'Outdoors': 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/OutdoorSplash%20(1).webp?v=1724091199820',
+    'Indoor Office': '/OfficeSplash.webp',
+    'Outdoors': '/OutdoorSplash.webp',
     '': 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/genericLoadingSplash.JPG?v=1724091328417' // generic loading in case of error
 }
 
@@ -12,12 +12,12 @@ export const environmentCards = [
     {
         id: 1,
         name: 'Outdoors', // The hover text content
-        imgSrc: 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/outdoorEnvCard.webp?v=1724091746123',
+        imgSrc: '/outdoorEnvCard.webp',
     },
     {
         id: 2,
         name: 'Indoor Office',
-        imgSrc: 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/indoorEnvCard.webp?v=1724091747127',
+        imgSrc: '/indoorEnvCard.webp',
     }
 ];
 
