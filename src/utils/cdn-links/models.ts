47ce7dea6088c0c1476965e8e61c398232b26ff8
rkmiller131 <rkmiller131@gmail.com>
2024-08-19 14:49:01 -0700

    Added models to ubiquity glitch

--- src/utils/cdn-links/models.ts
+++ src/utils/cdn-links/models.ts
@@ -1,7 +1,7 @@
-export const avatarModel = 'https://cdn.glitch.me/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/ManAvatar_Hair.vrm?v=1723065041817';
+export const avatarModel = 'https://cdn.glitch.me/155b1488-cef3-43d5-92c7-da25735e6c95/ManAvatar_Hair.vrm?v=1724102964867';
 
-export const officeModel = 'https://cdn.glitch.global/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/officeDoor-transformed.glb?v=1720632603947';
+export const officeModel = 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/IndoorOfficeModel.glb?v=1724103050803';
 
-export const groundPlane = 'https://cdn.glitch.global/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/GroundPlane.gltf?v=1722544682860';
+export const groundPlane = 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/GroundPlane.gltf?v=1724103109351';
 
-export const treeModel = 'https://cdn.glitch.global/22bbb2b4-7775-42b2-9c78-4b39e4d505e9/tree-transformed.glb?v=1715123611590';
+export const treeModel = 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/OutdoorTreeModel.glb?v=1724103170386';
