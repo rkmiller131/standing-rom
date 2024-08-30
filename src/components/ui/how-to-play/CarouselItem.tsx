b37a606e71b5d71b9b00064cd8dad2b6e5019eb1
rkmiller131 <rkmiller131@gmail.com>
2024-08-30 14:39:07 -0700

    on hover graphic card enlarges

--- src/components/ui/how-to-play/CarouselItem.tsx
+++ src/components/ui/how-to-play/CarouselItem.tsx
@@ -1,11 +1,47 @@
+import { useEffect, useRef } from 'react';
 import { H2PlayCard } from '../../../utils/cdn-links/motionGraphics';
-// import { gsap } from 'gsap';
+import { gsap } from 'gsap';
 
 interface CarouselItemProps {
     currentIndex: number;
     card: H2PlayCard;
 }
 export default function CarouselItem({ currentIndex, card }: CarouselItemProps) {
+    const elementRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        let element;
+        if (currentIndex === card.id) {
+            element = elementRef.current;
+        }
+        if (!element) return;
+
+        const animateHoverIn = () => {
+            gsap.to(element.querySelector('.instructional-video'), {
+                scale: 1.4,
+                y: '-20%',
+                duration: 0.5,
+            });
+        }
+
+        const animateHoverOut = () => {
+            gsap.to(element.querySelector('.instructional-video'), {
+                scale: 1,
+                y: '0%',
+                duration: 0.5,
+            });
+        }
+
+        element.addEventListener('mouseenter', animateHoverIn);
+        element.addEventListener('mouseleave', animateHoverOut);
+
+        return () => {
+            element.removeEventListener('mouseenter', animateHoverIn);
+            element.removeEventListener('mouseleave', animateHoverOut);
+        }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentIndex])
 
     const styles = {
         filter: currentIndex === card.id ? '' : 'blur(5px)',
@@ -14,11 +50,10 @@ export default function CarouselItem({ currentIndex, card }: CarouselItemProps) 
     }
 
     return (
-        <div id={`card${card.id}`} className="h2play-card" style={styles}>
+        <div id={`card${card.id}`} className="h2play-card" style={styles} ref={elementRef}>
             <video autoPlay loop muted className="instructional-video">
                 <source src={'/SHOULDER_FLEXION.webm'} />
             </video>
-            {/* {`CARD NUMBER ${card.id + 1}`} */}
         </div>
     );
 }
