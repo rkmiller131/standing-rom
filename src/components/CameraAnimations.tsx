2db07e45dcf49987d46e575160b040719a155c37
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-09-04 12:52:48 -0700

    ensure announcer is muted

--- src/components/CameraAnimations.tsx
+++ src/components/CameraAnimations.tsx
@@ -8,7 +8,7 @@ import useHookstateGetters from '../interfaces/Hookstate_Interface';
 export default function CameraAnimations() {
   const { camera } = useThree();
   const gameState = useGameState();
-  const { gameRunning } = useHookstateGetters();
+  const { gameRunning, getAnnouncer } = useHookstateGetters();
   const sideSpawned = gameState.levels[0].sideSpawned;
 
   useEffect(() => {
@@ -20,50 +20,62 @@ export default function CameraAnimations() {
         x: -2,
         y: 1,
         z: 0,
-        duration: 0.75
-      })
+        duration: 0.75,
+      });
       gsap.to(camera.rotation, {
         x: -2.1410783996934777,
         y: -1.75,
         z: -2.17339982566482,
-        duration: 0.75
+        duration: 0.75,
       });
       // final camera position (behind the avatar)
       gsap.to(camera.position, {
         x: 0,
         y: 1,
         z: 2,
-        duration: 0.75
-      })
+        duration: 0.75,
+      });
       gsap.to(camera.rotation, {
         x: 0,
         y: 0,
         z: 0,
-        duration: 0.75
+        duration: 0.75,
       });
     } else if (side === 'right' || side === 'left') {
       gsap.to(camera.position, {
         x: 0,
         y: 1,
         z: 2,
-        duration: 1.5
+        duration: 1.5,
       });
       gsap.to(camera.rotation, {
         x: 0,
         y: 0,
         z: 0,
-        duration: 1.5
-      })
+        duration: 1.5,
+      });
       // AUDIO ON CAMERA SWITCHES
       if (side === 'right' && gameRunning()) {
         const audio = new Audio(announcer['r_lateralRaise']);
         audio.volume = 0.75;
-        audio.play();
+        if (getAnnouncer()) {
+          audio.play();
+        } else if (getAnnouncer() === false) {
+          audio.pause();
+        } else {
+          audio.play();
+        }
       }
       if (side === 'left' && gameRunning()) {
         const audio = new Audio(announcer['l_lateralRaise']);
         audio.volume = 0.75;
-        audio.play();
+        if (getAnnouncer()) {
+          audio.play();
+        } else if (getAnnouncer() === false) {
+          audio.pause();
+        } else {
+          audio.play();
+        }
       }
     } else if (side === 'frontR' || side === 'frontL') {
       const invert = side === 'frontR' ? 1 : -1;
@@ -71,24 +83,36 @@ export default function CameraAnimations() {
         x: 1.4464595785701817 * invert,
         y: 1.6678777775434397,
         z: 0.57539653997066,
-        duration: 2
+        duration: 2,
       });
       gsap.to(camera.rotation, {
         x: -0.8574990047238096,
         y: 0.8992410888630242 * invert,
         z: 0.7354296422692501 * invert,
-        duration: 2
+        duration: 2,
       });
       // AUDIO ON CAMERA SWITCHES
       if (side === 'frontR' && gameRunning()) {
         const audio = new Audio(announcer['r_frontalRaise']);
         audio.volume = 0.75;
-        audio.play();
+        if (getAnnouncer()) {
+          audio.play();
+        } else if (getAnnouncer() === false) {
+          audio.pause();
+        } else {
+          audio.play();
+        }
       }
       if (side === 'frontL' && gameRunning()) {
         const audio = new Audio(announcer['l_frontalRaise']);
         audio.volume = 0.75;
-        audio.play();
+        if (getAnnouncer()) {
+          audio.play();
+        } else if (getAnnouncer() === false) {
+          audio.pause();
+        } else {
+          audio.play();
+        }
       }
     } else if (side === 'crossR' || side === 'crossL') {
       const invert = side === 'crossR' ? 1 : -1;
@@ -96,28 +120,40 @@ export default function CameraAnimations() {
         x: 0.6806856113154883 * invert,
         y: 2.271391391050578,
         z: 0.5156858327599767,
-        duration: 2
+        duration: 2,
       });
       gsap.to(camera.rotation, {
         x: -0.9833133808337716,
         y: 0.3391550045698274 * invert,
         z: 0.46332582886228135 * invert,
-        duration: 2
+        duration: 2,
       });
       // AUDIO ON CAMERA SWITCHES
       if (side === 'crossR' && gameRunning()) {
         const audio = new Audio(announcer['r_crossBody']);
         audio.volume = 0.75;
-        audio.play();
+        if (getAnnouncer()) {
+          audio.play();
+        } else if (getAnnouncer() === false) {
+          audio.pause();
+        } else {
+          audio.play();
+        }
       }
       if (side === 'crossL' && gameRunning()) {
         const audio = new Audio(announcer['l_crossBody']);
         audio.volume = 0.75;
-        audio.play();
+        if (getAnnouncer()) {
+          audio.play();
+        } else if (getAnnouncer() === false) {
+          audio.pause();
+        } else {
+          audio.play();
+        }
       }
     }
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sideSpawned]);
 
   return null;
