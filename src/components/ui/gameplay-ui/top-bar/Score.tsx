3a02800f8ed94fd75866e1bcab75c52e29d5a921
rkmiller131 <rkmiller131@gmail.com>
2024-09-04 13:02:14 -0700

    Added hardcoded arm angle under score display

--- src/components/ui/gameplay-ui/top-bar/Score.tsx
+++ src/components/ui/gameplay-ui/top-bar/Score.tsx
@@ -25,9 +25,55 @@ export default function Score () {
       <div className={`player-score ${streaking ? 'streaking-score': 'frosted-glass'}`}>
         <span className="player-score-content">{`${popped} / ${total}`}</span>
       </div>
-      <div className="angle-readout">
-        Second Div
+      <div className="angle-readout-container">
+        <div className="angle-readout-item">
+          <span>Left ROM</span>
+          <span className="angle-measure">
+            150°
+          </span>
+        </div>
+        <div className="angle-readout-item">
+          <span>Right ROM</span>
+          <span className="angle-measure">
+            45°
+          </span>
+        </div>
       </div>
     </div>
   )
 }
+
+// import { useEffect, useState } from 'react';
+// import calcArmAngles from '../math/calcArmAngles';
+// import { VRM } from '@pixiv/three-vrm';
+
+// import '../../css/ViewScreen.css';
+
+// interface ProProps {
+//   avatar: React.RefObject<VRM>;
+// }
+
+// export default function Protractor({ avatar }: ProProps) {
+//   const [la, setLa] = useState(0);
+//   const [ra, setRa] = useState(0);
+
+//   useEffect(() => {
+//     const intervalId = setInterval(() => {
+//       const { leftArmAngle, rightArmAngle } = calcArmAngles(avatar);
+
+//       setLa(leftArmAngle);
+//       setRa(rightArmAngle);
+//     }, 500);
+
+//     return () => clearInterval(intervalId);
+//   }, [avatar]);
+
+//   return (
+//     <>
+//       <div>
+//         <p className="vbt">Right: {ra} °</p>
+//         <p className="vbt">Left: {la} °</p>
+//       </div>
+//     </>
+//   );
+// }
