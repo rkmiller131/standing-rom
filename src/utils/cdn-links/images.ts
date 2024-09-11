8055761c77885282f134f3b29b92ae66cde0ba08
rkmiller131 <rkmiller131@gmail.com>
2024-09-10 20:26:42 -0700

    Still fixing cdn mismatch

--- src/utils/cdn-links/images.ts
+++ src/utils/cdn-links/images.ts
@@ -41,24 +41,17 @@ export const lockedIcons = {
 }
 
 export const calibrationIcons = {
-  calibrating:
-    'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/yellowCalibIcon%20(1).png?v=1724093926378',
-  calibrated:
-    'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/blueCalibIcon%20(1).png?v=1724093823488',
+  calibrating: 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/yellowCalibIcon%20(1).png?v=1724093926378',
+  calibrated: 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/blueCalibIcon%20(1).png?v=1724093823488',
 };
 
 export const uvxLogos: { [key: string]: string } = {
-  uvxColor:
-    'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/uvxLogoColor%20(1).png?v=1724094223669',
-  uvxWhite:
-    'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/uvxLogoWhite.png?v=1724094162122',
+  uvxColor: 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/uvxLogoColor%20(1).png?v=1724094223669',
+  uvxWhite: 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/uvxLogoWhite.png?v=1724094162122',
 };
 
 export const badgesIcons = {
-  bronzeMedal:
-    'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/bronzeMedal.webp?v=1724098067340',
-  silverMedal:
-    'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/silverMedal.webp?v=1724098064685',
-  goldMedal:
-    'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/goldMedal.webp?v=1724098061765',
+  bronzeMedal: 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/bronzeMedal.webp?v=1724098067340',
+  silverMedal: 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/silverMedal.webp?v=1724098064685',
+  goldMedal: 'https://cdn.glitch.global/155b1488-cef3-43d5-92c7-da25735e6c95/goldMedal.webp?v=1724098061765',
 };
