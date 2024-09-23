f3d94dc227569b14a311af132f5b201b3f4570ea
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2024-09-23 16:48:18 -0700

    prepare room code verification

--- src/components/ui/UIElements.tsx
+++ src/components/ui/UIElements.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useState } from 'react';
+import { lazy, Suspense, useCallback, useState } from 'react';
 import { VRM } from '@pixiv/three-vrm';
 import useHookstateGetters from '../../interfaces/Hookstate_Interface';
 import LoadingScreen from './LoadingScreen';
@@ -8,6 +8,7 @@ import SlidingInfo from './SlidingInfo';
 import GameInstructions from './how-to-play/GameInstructions';
 import RoomCode from './RoomCode';
 import GameSetupScreen from './game-setup/GameSetupScreen';
+import { getSessionInformation } from '../../utils/http/httpStoreCode';
 
 const GameplayUI = lazy(() => import('./gameplay-ui/GameplayUI'));
 
@@ -16,18 +17,36 @@ interface UIProps {
 }
 
 export default function UIElements({ avatar }: UIProps) {
-  const { environmentLoaded, environmentSelected, sceneLoaded, gameOver } =
-    useHookstateGetters();
+  const {
+    environmentLoaded,
+    environmentSelected,
+    sceneLoaded,
+    gameOver,
+    setGameID,
+    setRoomCode,
+  } = useHookstateGetters();
   const [codeSuccess, setCodeSuccess] = useState(false);
   const [consentGiven, setConsentGiven] = useState(false);
 
   // future optimizations: put these fn inside a useCallback
-  const submitCode = (code: number) => {
-    // be sure to add some validation logic here if necessary when backend is established
-    // add a new helper function to utils > http and verify against database
-    console.log('Code submitted: ', code);
-    setCodeSuccess(true);
-  };
+  const submitCode = useCallback(
+    async (code: number) => {
+      // be sure to add some validation logic here if necessary when backend is established
+      // add a new helper function to utils > http and verify against database
+      try {
+        const results = await getSessionInformation({ code: code });
+
+        console.log('Code submitted: ', code, results);
+
+        setRoomCode(results.code);
+        setGameID(results._id);
+        setCodeSuccess(true);
+      } catch (error) {
+        console.error('Error submitting code:', error);
+      }
+    },
+    [setGameID, setRoomCode],
+  ) as (code: number) => void;
 
   const clientGrantsConsent = () => {
     setConsentGiven(true);
